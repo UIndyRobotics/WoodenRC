7477d0c77e3afdc952f0f3f7efa20f023d5534e7
Clayton <claytonwinders@gmail.com>
2018-04-09 18:40:44 -0400

    Add some possible code for timer

--- Photon-Racecar/UDP-Gate.py
+++ Photon-Racecar/UDP-Gate.py
@@ -2,7 +2,7 @@
   UDP-Network-Photon.py
   Clayton Winders
   March 29, 2018
-  Prints UDP packets send to this machine, port '8081'.  Unpackes a struct created on a
+  Prints UDP packets send to this machine and port.  Unpackes a struct created on a
   Particle Photon with the following:
 
   struct network_info_t{
@@ -28,7 +28,7 @@
     char subnet_mask[16];
     byte access_point_BSSID[6];
     char ssid[20];
-
+    char terminator[4];
 };
 
   See udp-wifi-streamer.ino for the Photon code.
@@ -36,46 +36,44 @@
 
 port = 49154
 
-struct_format = 'IiiiIifffff6s16s16s16s6s20s'
+struct_format = 'IiiiIifffff6s16s16s16s6s20s4s'
 throttle = -1
 steer = -1
 
 import os
 from socket import *
 from struct import *
+import datetime, time
 
+seconds = 0
 def getMAS(mac):
   return "%x:%x:%x:%x:%x:%x" % unpack("BBBBBB",mac)
 
 host = ""
-
+lapTime = 60
 buf = 1024
 addr = (host, port)
 UDPSock = socket(AF_INET, SOCK_DGRAM)
 UDPSock.bind(addr)
 print("Waiting to receive message...")
+ir_changes = 0
 while True:
-    (data, addr) = UDPSock.recvfrom(buf)
-    #data = data.decode('utf-8')
-    print(len(data), calcsize(struct_format))
-    #(device_mac, local_ip, gateway_ip, subnet_mask, sig_strength, bssid, ssid, counter, throttle, steer) = unpack("6s16s16s16si6s20siii", data)
-    (counter, throttle, throttle_out, steer, ir_changes, sig_strength, ax, ay, az, battery_voltage_in, battery_current_in,device_mac, local_ip, gateway_ip, subnet_mask, bssid, ssid) = unpack(struct_format, data)
+    #(data, addr) = UDPSock.recvfrom(buf)
+    #print(len(data), calcsize(struct_format))
+    #(counter, throttle, throttle_out, steer, ir_changes, sig_strength, ax, ay, az, battery_voltage_in, battery_current_in,device_mac, local_ip, gateway_ip, subnet_mask, bssid, ssid) = unpack(struct_format, data)
+    #print("IR_changes: ", ir_changes)
+    if(ir_changes > 0 & seconds == 0):
+      ir_last = ir_changes
+      now = datetime.datetime.now()
+      seconds = now.hour * 3600 + (now.minute * 60) + now.second + now.microsecond/1000000
+      start_time = seconds
+    if(seconds not = 0):
+      now = datetime.datetime.now()
+      seconds = now.hour * 3600 + (now.minute * 60) + now.second + now.microsecond/1000000
+    if(ir_changes > ir_last & seconds > (seconds+lapTime))
+    
 
-#     local_ip = local_ip.rstrip('\0')
-#     gateway_ip = gateway_ip.rstrip('\0')
-#     subnet_mask = subnet_mask.rstrip('\0')
-#     ssid = ssid.rstrip('\0')
-
-    print("local: %s\ngateway: %s\nmask: %s\nstrength: %i\nssid: %s\ncounter: %i\nmac: %s\nbssid: %s\nthrottle: %s\nthrottle_out: %s\nsteer: %s" % (local_ip, gateway_ip, subnet_mask, sig_strength, ssid, counter, getMAS(device_mac), getMAS(bssid), throttle, throttle_out, steer))
-    print("ax: ", ax)
-    print("ay: ", ay)
-    print("az: ", az)
-    print("Battery Voltage: ", battery_voltage_in)
-    print("Battery Current: ", battery_current_in)
-    print("IR_changes: ", ir_changes)
-    #print unpacked
-    #print("Received message (" + str(len(data)) + "): " + data)
-
+    
 
 '''
 
