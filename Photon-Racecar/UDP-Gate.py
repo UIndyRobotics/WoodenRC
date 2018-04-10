08552940cd6d27a13773c348f43fc2626f562fdb
Clayton <claytonwinders@gmail.com>
2018-04-09 22:17:45 -0400

    Take in TCP and count for laps

--- Photon-Racecar/UDP-Gate.py
+++ Photon-Racecar/UDP-Gate.py
@@ -33,9 +33,9 @@
 
   See udp-wifi-streamer.ino for the Photon code.
 '''
-
+host = "18.217.55.123"  # IP address of Talaga's EC2 repeater
 port = 49154
-
+addr = (host, port)
 struct_format = 'IiiiIifffff6s16s16s16s6s20s4s'
 throttle = -1
 steer = -1
@@ -49,46 +49,56 @@ seconds = 0
 def getMAS(mac):
   return "%x:%x:%x:%x:%x:%x" % unpack("BBBBBB",mac)
 
-host = ""
 ir_last = 0
-lapTime = 2
+lapTime = 2.0*1000
 lapCount = 0
 lapTimes = []
 buf = 1024
-addr = (host, port)
-UDPSock = socket(AF_INET, SOCK_DGRAM)
-UDPSock.bind(addr)
+TCPSock = socket(AF_INET, SOCK_STREAM)
+TCPSock.connect(addr)
 print("Waiting to receive message...")
 ir_changes = 0
 start_time = 0
 numLaps = 4
+data = ""
+total = 0
+currentMilliTime = lambda: int(round(time.time() * 1000))
 while True:
-  try:
-    #(data, addr) = UDPSock.recvfrom(buf)
+    data = data + TCPSock.recv(buf)
+    if len(data) < calcsize(struct_format):
+      continue
+    if len(data) > calcsize(struct_format):
+      data = data[-calcsize(struct_format):]
     #print(len(data), calcsize(struct_format))
-    #(counter, throttle, throttle_out, steer, ir_changes, sig_strength, ax, ay, az, battery_voltage_in, battery_current_in,device_mac, local_ip, gateway_ip, subnet_mask, bssid, ssid) = unpack(struct_format, data)
+    (counter, throttle, throttle_out, steer, ir_changes, sig_strength, ax, ay, az, battery_voltage_in, battery_current_in,device_mac, local_ip, gateway_ip, subnet_mask, bssid, ssid, terminator) = unpack(struct_format, data)
     #print("IR_changes: ", ir_changes)
-    if((ir_changes > 0.0) & (seconds == 0.0)):
+    
+    if((ir_changes > 0.0) and (lapCount == 0)):
       ir_last = ir_changes
-      print("Starting")
-      now = datetime.datetime.now()
-      seconds = now.hour * 3600 + (now.minute * 60) + now.second + now.microsecond/1000000
-      start_time = seconds
+      start_time = currentMilliTime()
+      print("Starting", currentMilliTime(), ir_changes)
       lapCount = 1
-      lapTimes.append(seconds)
-    if(seconds > 0.0):
-      now = datetime.datetime.now()
-      seconds = now.hour * 3600 + (now.minute * 60) + now.second + now.microsecond/1000000
-    if((ir_changes > ir_last) & (seconds > (start_time+lapTime))):
-      print("Incrementing")
+      lapTimes.append(currentMilliTime())
+    if((ir_changes > ir_last) and (currentMilliTime() > (lapTimes[-1]+lapTime))):
       lapCount = lapCount + 1
-      lapTimes.append(seconds)
+      lapTimes.append(currentMilliTime())
       ir_last = ir_changes
+      print("Incrementing", currentMilliTime(), ir_changes )
     if(lapCount == (numLaps)):
-      print(lapTimes)
+      laps = 0
+      for lap in lapTimes:
+        if laps == 0:
+          print(0)
+          old = lap
+          laps = laps + 1
+        else:
+          this = (lap-old)/1000.0
+          print(this)
+          old = lap
+          total = total + this
+      print(total)
       break
-  except KeyboardInterrupt:
-    ir_changes = ir_changes + 1
+
     
 
     
