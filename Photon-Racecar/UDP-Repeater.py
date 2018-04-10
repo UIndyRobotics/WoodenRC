fbde6ce1f5132bd50d5c4e747f15dde3a618baca
EC2 Default User <ec2-user@ip-172-31-18-33.us-east-2.compute.internal>
2018-04-10 00:20:47 +0000

    Added record playback

--- Photon-Racecar/UDP-Repeater.py
+++ Photon-Racecar/UDP-Repeater.py
@@ -16,6 +16,9 @@ from socket import *
 from struct import *
 import select
 from threading import Thread
+import time
+
+currentMilliTime = lambda: int(round(time.time() * 1000))
 
 class TCPThread(Thread):
   def __init__(self, con_list):
@@ -40,6 +43,14 @@ class TCPThread(Thread):
 
 host = ""
 
+packet_capture = []
+start_play_time = -1
+mode = 0
+playback_i = 0
+# 0 = no
+# 1 = recording - 'record' - clears old data
+# 2 = playback  - 'play'
+
 buf = 1024
 udp_addr = (host, UDP_port)
 UDPSock = socket(AF_INET, SOCK_DGRAM)
@@ -53,21 +64,58 @@ tcp_thread.start()
 
 print("Waiting to receive message...")
 while True:
-    (data, addr) = UDPSock.recvfrom(buf)
-    (counter) = unpack('i', data[0:4]) # pull the first integer off
-#    print("Got UDP len: %d id: %d listeners: %d" % ( len(data), counter[0], len(con_list)))
-    #print "%d %d" % (counter[0], len(con_list))
-    # Loop through all the connections and send to each client
-    # TODO: This may cause a slowdown, investigate, and possibly multithread.
+    if mode == 0 or mode == 1: #no recording or recording
+      (data, addr) = UDPSock.recvfrom(buf)
+      (counter) = unpack('i', data[0:4]) # pull the first integer off
+      #    print("Got UDP len: %d id: %d listeners: %d" % ( len(data), counter[0], len(con_list)))
+      #print "%d %d" % (counter[0], len(con_list))
+      # Loop through all the connections and send to each client
+      # TODO: This may cause a slowdown, investigate, and possibly multithread.
+    elif mode == 2: #playback
+      (t, data) = packet_capture[playback_i]
+      while t + start_play_time > currentMilliTime():
+        pass
+      playback_i += 1
+      if playback_i >= len(packet_capture):
+        print("End of playback")
+        mode = 0
+
+    if mode == 1:
+      packet_capture.append( (currentMilliTime() - start_play_time, data) )
+      print(len(packet_capture))
+
+    if len(packet_capture) > 100000:
+      print("Too much data recorded")
+      mode = 0
+    # Send data
+
     for c in con_list:
       (r,w,e) = select.select((c,), (), (), 0)
       # select r returns an empty list if the socket is ready to send.
+
+      # No data coming in on TCP
       if len(r) == 0:
         c.send(data)
-      else:
-        print "Removing TCP client"
-        con_list.remove(c)
-        
+      if len(r) == 1:
+        user_input = c.recv(buf)
+        if len(user_input) == 0:
+          print("Removing TCP client")
+          con_list.remove(c)
+        else:
+          c.send(data)
+          if 'record' in user_input:
+            print("Recording")
+            packet_capture = []
+            packet_capture.append( (0 , data) ) # tuples of (time from start of capture, data)
+            start_play_time = currentMilliTime()
+            mode = 1
+          elif 'play' in user_input:
+            print("Playback")
+            mode = 2
+            playback_i = 0
+            start_play_time = currentMilliTime()
+          else:
+            print("%s not understoon!" % (user_input))
 
 tcp_thread.join()
 
