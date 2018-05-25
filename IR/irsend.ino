d776bd144ea1bff64a895051247f35ba596959ad
Clayton Winders <claytonwinders@gmail.com>
2018-05-25 04:13:46 +0000

    Tuned to 38khz added on/off

--- IR/irsend.ino
+++ IR/irsend.ino
@@ -1,30 +1,23 @@
 
-#define Duty_Cycle 50  //in percent (10->50), usually 33 or 50
+#define Duty_Cycle 40  //in percent (10->50), usually 33 or 50
 //actual duty cycle will be relative close to set value, depending on carrier freq
 
 #define Carrier_Frequency 38000   //usually one of 38000, 40000, 36000, 56000, 33000, 30000
-
+// PERIOD is 26.3usec for 38khz
 
 #define PERIOD    (1000000+Carrier_Frequency/2)/Carrier_Frequency
+
 #define HIGHTIME  PERIOD*Duty_Cycle/100
 #define LOWTIME   PERIOD - HIGHTIME
 #define txPinIR   D6   //IR carrier output pin
 
 #define LED D7
 
-
-
-unsigned long sigTime = 0; //use in mark & space functions to keep track of time
-
-//RAW NEC signal -32 bit with 1 repeat - make sure buffer starts with a Mark
-unsigned int NEC_RAW[] = {9000, 4500, 560, 560, 560, 560, 560, 1690, 560, 560, 560, 560, 560, 560, 560, 560, 560, 560, 560, 1690, 560, 1690, 560, 560, 560, 1690, 560, 1690, 560, 1690, 560, 1690, 560, 1690, 560, 560, 560, 560, 560, 1690, 560, 560, 560, 560, 560, 560, 560, 1690, 560, 560, 560, 1690, 560, 1690, 560, 560, 560, 1690, 560, 1690, 560, 1690, 560, 560, 560, 1690, 560, 39980, 9000, 2232, 560}; //AnalysIR Batch Export (IRremote) - RAW
-
-//RAW Mitsubishi 88 bit signal  - make sure buffer starts with a Mark
-unsigned int Mitsubishi_RAW[] = {3172, 1586, 394, 394, 394, 1182, 394, 394, 394, 394, 394, 1182, 394, 394, 394, 1182, 394, 394, 394, 394, 394, 1182, 394, 1182, 394, 1182, 394, 394, 394, 1182, 394, 394, 394, 1182, 394, 1182, 394, 1182, 394, 394, 394, 394, 394, 394, 394, 394, 394, 1182, 394, 1182, 394, 394, 394, 1182, 394, 1182, 394, 394, 394, 394, 394, 1182, 394, 394, 394, 394, 394, 1182, 394, 394, 394, 394, 394, 1182, 394, 1182, 394, 394, 394, 1182, 394, 1182, 394, 1182, 394, 1182, 394, 1182, 394, 1182, 394, 1182, 394, 1182, 394, 1182, 394, 1182, 394, 394, 394, 394, 394, 394, 394, 394, 394, 394, 394, 394, 394, 394, 394, 394, 394, 1182, 394, 1182, 394, 1182, 394, 1182, 394, 1182, 394, 1182, 394, 394, 394, 1182, 394, 394, 394, 394, 394, 394, 394, 394, 394, 394, 394, 394, 394, 1182, 394, 394, 394, 394, 394, 1182, 394, 1182, 394, 1182, 394, 1182, 394, 1182, 394, 394, 394, 1182, 394, 1182, 394, 394, 394, 394, 394, 394, 394, 394, 394, 394, 394, 1182, 394, 394, 394}; //AnalysIR Batch Export (IRremote) - RAW
+unsigned long sigLastStop = 0; //use in mark & space functions to keep track of time
 
 
 void setup() {
-   WiFi.on();
+   //WiFi.on();
   //  Serial.begin(57600);
   pinMode(txPinIR, OUTPUT);
   pinMode(LED, OUTPUT);
@@ -34,42 +27,38 @@ void setup() {
   
   //make sure there is enough time to reflash.
   delay(1000);
-  delay(1000);
-  delay(1000);
-  delay(1000);
-  delay(1000);
-  delay(1000);
-  delay(1000);
-  delay(1000);
-  delay(1000);
+  
+  sigLastStop = micros();
 
 }
 
 void loop() {
-  for (int i=0;i<10;i++){
-    mark(500);
-    space(1000);
+  for (int i=0;i<7500;i++){ // should finish every second
+    mark(1000);
+    space(500);
   }
-  delay(1000);
 }
 
-void mark(unsigned int mLen) { //uses sigTime as end parameter
-  sigTime+= mLen; //mark ends at new sigTime
-  unsigned long now = micros();
-  unsigned long dur = sigTime - now; //allows for rolling time adjustment due to code execution delays
-  if (dur == 0) return;
-  while ((micros() - now) < dur) { //just wait here until time is up
-    digitalWrite(txPinIR, HIGH);
+
+void mark(unsigned long mLen) {
+  sigLastStop += mLen; //allows for rolling time adjustment due to code execution delays
+  while (micros()  < sigLastStop) { //just wait here until time is up
+    digitalWrite(txPinIR, HIGH); // 30u-sec
     delayMicroseconds(HIGHTIME - 3);
-    digitalWrite(txPinIR, LOW);
+    digitalWrite(txPinIR, LOW);  // 40u-sec
     delayMicroseconds(LOWTIME - 4);
+    digitalWrite(txPinIR, HIGH); // 30u-sec
+    delayMicroseconds(HIGHTIME - 3);
+    digitalWrite(txPinIR, LOW);  // 40u-sec
+    delayMicroseconds(LOWTIME - 4);
+    digitalWrite(txPinIR, HIGH); // 30u-sec
+    delayMicroseconds(HIGHTIME - 3);
+    digitalWrite(txPinIR, LOW);  // 40u-sec
+    delayMicroseconds(LOWTIME - 5);
   }
 }
 
-void space(unsigned int sLen) { //uses sigTime as end parameter
-  sigTime+= sLen; //space ends at new sigTime
-  unsigned long now = micros();
-  unsigned long dur = sigTime - now; //allows for rolling time adjustment due to code execution delays
-  if (dur == 0) return;
-  while ((micros() - now) < dur) ; //just wait here until time is up
+void space(unsigned long mLen) { //uses sigTime as end parameter
+  sigLastStop += mLen; //allows for rolling time adjustment due to code execution delays
+  while (micros()  < sigLastStop);// { //just wait here until time is up
 }
