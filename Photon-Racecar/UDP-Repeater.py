8244e61ac82c797975ea3bfe6cd7eb336df96801
Paul Graham Talaga <talagap@csci01.is.uindy.edu>
2018-05-30 16:28:49 -0400

    added exception, remove playback

--- Photon-Racecar/UDP-Repeater.py
+++ Photon-Racecar/UDP-Repeater.py
@@ -64,31 +64,8 @@ tcp_thread.start()
 
 print("Waiting to receive message...")
 while True:
-    if mode == 0 or mode == 1: #no recording or recording
-      (data, addr) = UDPSock.recvfrom(buf)
-      #data = ""
-#      (counter) = unpack('i', data[0:4]) # pull the first integer off
-      #    print("Got UDP len: %d id: %d listeners: %d" % ( len(data), counter[0], len(con_list)))
-      #print "%d %d" % (counter[0], len(con_list))
-      # Loop through all the connections and send to each client
-      # TODO: This may cause a slowdown, investigate, and possibly multithread.
-    elif mode == 2: #playback
-      (t, data) = packet_capture[playback_i]
-      while t + start_play_time > currentMilliTime():
-        pass
-      playback_i += 1
-      if playback_i >= len(packet_capture):
-        print("End of playback")
-        mode = 0
-
-    if mode == 1:
-      packet_capture.append( (currentMilliTime() - start_play_time, data) )
-      print(len(packet_capture))
-
-    if len(packet_capture) > 100000:
-      print("Too much data recorded")
-      mode = 0
-    # Send data
+    #if mode == 0 or mode == 1: #no recording or recording
+    (data, addr) = UDPSock.recvfrom(buf)
 
     for c in con_list:
       (r,w,e) = select.select((c,), (), (), 0)
@@ -97,28 +74,18 @@ while True:
       # No data coming in on TCP
       if len(r) == 0:
         c.send(data)
+        #print("sent")
       if len(r) == 1:
-        user_input = c.recv(buf)
+        user_input = ""
+        try:
+          user_input = c.recv(buf)
+        except:
+          user_input = ""
         if len(user_input) == 0:
           print("Removing TCP client")
           con_list.remove(c)
         else:
           c.send(data)
-          # chomp incoming tcp data
-          user_input = ''
-          if 'r' in user_input:
-            print("Recording")
-            packet_capture = []
-            packet_capture.append( (0 , data) ) # tuples of (time from start of capture, data)
-            start_play_time = currentMilliTime()
-            mode = 1
-          elif 'p' in user_input:
-            print("Playback")
-            mode = 2
-            playback_i = 0
-            start_play_time = currentMilliTime()
-          else:
-            print("%s not understoon!" % (user_input))
 
 tcp_thread.join()
 
